7d23572a45d80986ff693ff2ad80acf2603bd5e2
Brett Gonterman <brettgonterman@sbcglobal.net>
2018-06-03 21:50:36 -0500

    star wars

--- assets/javascripts/Move.js
+++ assets/javascripts/Move.js
@@ -1,6 +1,10 @@
  function myMove(clss) {
     
      var pos = 0;
+     var posEn1=0;
+     var posEn2=0;
+     var posEn3=0;
+     var posEn4=0;
     // switch(clss) {
     //     case "charOne":
     //     var hero = document.getElementsByClassName(characters+"One")[0];
@@ -34,8 +38,10 @@
             baseAttackPoints:9,
             heroMovement:80,
             villanMovement:50,
-            character: document.getElementsByClassName("charOne")[0],
-            move:setInterval(function(){ frame(char.char1.character,this.constructor.name) }, 3)
+            //character: document.getElementsByClassName("charOne")[0],
+            character: $(".charOne")[0]
+            //character: $(".charOne").attr('class')
+            // move:setInterval(function(){ frame(char.char1.character,this.constructor.name) }, 3)
         },
         char2 :{
             name: "Lando Calrissian",
@@ -43,7 +49,7 @@
             baseAttackPoints:10,
             heroMovement:80,
             villanMovement:50,
-            character: document.getElementsByClassName("charTwo")[0]
+            character: $(".charTwo")[0]
         },
         char3 :{
             name: "Chewbacca",
@@ -51,7 +57,7 @@
             baseAttackPoints:15,
             heroMovement:80,
             villanMovement:50,
-            character: document.getElementsByClassName("charThree")[0]
+            character: $(".charThree")[0]
         },
         char4 :{
             name:"Greedo",
@@ -59,7 +65,7 @@
             baseAttackPoints:5,
             heroMovement:80,
             villanMovement:50,
-            character: document.getElementsByClassName("charFour")[0]
+            character: $(".charFour")[0]
         },
         char5 :{
             name:"kitonak",
@@ -67,26 +73,89 @@
             baseAttackPoints:20,
             heroMovement:80,
             villanMovement:50,
-            character: document.getElementsByClassName("charFive")[0]
+            character: $(".charFive")[0]
          }
         }
-    }
-    
+        
+       var clssName= $("."+clss).attr('class');
+       clssName=="charOne"?(movement=80,id = setInterval(function(){ frame(char.char1.character,movement) }, 3),enemyId1 = setInterval(function(){ enemy1(char.char2.character) }, 3),enemyId2 = setInterval(function(){ enemy2(char.char3.character) }, 3),enemyId3 = setInterval(function(){ enemy3(char.char4.character) }, 3),enemyId4 = setInterval(function(){ enemy4(char.char5.character) }, 3)):(clssName=="charTwo"?(movement=80,id = setInterval(function(){ frame(char.char2.character,movement) }, 3),enemyId1 = setInterval(function(){ enemy1(char.char1.character) }, 3),enemyId2 = setInterval(function(){ enemy2(char.char3.character) }, 3),enemyId3 = setInterval(function(){ enemy3(char.char4.character) }, 3),enemyId4 = setInterval(function(){ enemy4(char.char5.character) }, 3)):( movement=50,id = setInterval(function(){ frame(char.char2.character,movement) }, 3)))
+        //var enemyId = setInterval(function(){ enemy(char.char2.character) }, 3);
         // var hero = document.getElementsByClassName(clss)[0];  
         
 
-    // var id = setInterval(function(){ frame(hero) }, 3);
-    // function frame(elem,guy) {
-    //     alert(elem);
-    //     var characters=["charOne","charTwo","charThree","charFour","charFive"];
-    //     movement=80;
-    //     upOrDown=pos++
-    //     if (upOrDown == movement) {
-    //     clearInterval(id);
-    //     } else {
-    //         elem.style.position ='relative';
-    //         elem.style.top=upOrDown+"px";
+    //var id = setInterval(function(){ frame(hero) }, 3);
+    function frame(elem,movement) {
+       // alert(pos);
+        //var characters=["charOne","charTwo","charThree","charFour","charFive"];
+        //var pos=0;
+       // movement=80;
+        upOrDown=pos++;
+        //console.log(upOrDown);
+        if (upOrDown == movement) {
+        clearInterval(id);
+        } 
+        else {
+            elem.style.position ='relative';
+            elem.style.top=upOrDown+"px";
          
-    //         }
-    //     }  
-   
+            }
+            
+        }  
+
+        function enemy1(elem,movement) {
+            movement=50;
+             upOrDown=posEn1++;
+             //console.log(upOrDown);
+             if (upOrDown == movement) {
+             clearInterval(enemyId1);
+             } 
+             else {
+                 elem.style.position ='relative';
+                 elem.style.top=upOrDown+"px";
+              
+                 }
+                 
+             }  
+             function enemy2(elem,movement) {
+                movement=50;
+                 upOrDown=posEn2++;
+                 //console.log(upOrDown);
+                 if (upOrDown == movement) {
+                 clearInterval(enemyId2);
+                 } 
+                 else {
+                     elem.style.position ='relative';
+                     elem.style.top=upOrDown+"px";
+                  
+                     }
+                     
+                 } 
+                 function enemy3(elem,movement) {
+                    movement=50;
+                     upOrDown=posEn3++;
+                     //console.log(upOrDown);
+                     if (upOrDown == movement) {
+                     clearInterval(enemyId3);
+                     } 
+                     else {
+                         elem.style.position ='relative';
+                         elem.style.top=upOrDown+"px";
+                      
+                         }
+                         
+                     }   
+                     function enemy4(elem,movement) {
+                        movement=50;
+                         upOrDown=posEn4++;
+                         //console.log(upOrDown);
+                         if (upOrDown == movement) {
+                         clearInterval(enemyId4);
+                         } 
+                         else {
+                             elem.style.position ='relative';
+                             elem.style.top=upOrDown+"px";
+                          
+                             }
+                             
+                         }   
+   }
